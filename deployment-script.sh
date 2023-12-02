fdef31c2ef74f52bf57c26503e819b50ab4d1a9c
Dipu Ranjan <dipuranjanec08@gmail.com>
2023-12-02 05:43:02 +0530

    added deployment script

---
+++ deployment-script.sh
@@ -0,0 +1,7 @@
+composer update --optimize-autoloader
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
+php artisan optimize
+php artisan storage:link
